c0ea151f3f0846cca6ecff7f0dd297a323a4f653
ansonliang55 <ansonlia@cs.toronto.edu>
2015-03-31 01:56:21 -0400

    some restructuring

--- src/data_train.py
+++ src/data_train.py
@@ -12,7 +12,7 @@ import superpixel as sp
 import slic as sl
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
-from sklearn import preprocessing
+from sklearn.naive_bayes import GaussianNB
 #constant
 TRAINING_LABEL=0
 VALIDATION_LABEL=1
@@ -29,6 +29,7 @@ valid_labels = data['valid_labels']
 scaler = StandardScaler()
 #clf = SGDClassifier(loss="log", penalty="l2")
 #clf = SGDRegressor(loss="squared_loss")
+#clf = GaussianNB()
 clf = svm.SVC(kernel='rbf', probability=True)
 scaler.fit(train_data)
 train_data = scaler.transform(train_data)
@@ -45,32 +46,26 @@ for i in xrange(0,total_sample):
 		if clf.predict(valid_data[i]) == valid_labels[i]:
 				count_correct+=1
 
-print 1.0*count_correct/total_sample
-#print clf.coef_
+#print ('Validation Accuracy: %2.2f%%')%100.0*count_correct/total_sample
+
 
 sp_file_names = data['sp_file_names'][0].strip()
 im_file_names = data['im_file_names'][0].strip()
-
-
 # read input image
 image = img_as_float(io.imread(im_file_names))
-
 # get slic superpixel segmentation
 im_sp = sl.getSlicSuperpixels(image, numSegments, com_factor)
 #im_sp = scipy.io.loadmat(sp_file_names)['labels']
-
 # get superpixel centroid location
 sp_location = sp.getSuperPixelLocations(im_sp)
-
 # get superpixel mean color		
 sp_color = sp.getSuperPixelMeanColor(im_sp, image)
-
 # get superpixel size
 sp_size = sp.getSuperPixelSize(im_sp)
-		
 test_data = np.vstack((sp_location.T,sp_color.T, sp_size.T)).T
+
 test_data = scaler.transform(test_data)
 sp.showPrediction(clf, im_sp, test_data, image)
 
-#scipy.io.savemat('data.mat', {'train_data':train_data, 'valid_data':valid_data, 'train_labels':train_labels, 'valid_labels':valid_labels, 'file_labels':file_labels, 'im_file_names':im_file_names, 'sp_file_names':sp_file_names, 'label_file_names':label_file_names}, oned_as='column')
+
 
