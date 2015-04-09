c4f02249c726616634761beebac2b4d9242e5abe
ansonliang55 <ansonlia@cs.toronto.edu>
2015-04-09 05:33:30 -0400

    more indentation fix file fix add pdf

---
+++ src/data_preprocessing.py
@@ -0,0 +1,12 @@
+"""
+Written by: Wenjie Zi
+
+This is the handler of data preprocessing
+"""
+
+#@input: training data,
+#@input: validation data
+#@input: test data
+#@output: the training data, validation data and test data after reducing dimensions
+import numpy as np
+
