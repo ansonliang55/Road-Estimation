c132437fba7aabbbcca0169931c730176e039171
ansonliang55 <ansonlia@cs.toronto.edu>
2015-04-12 06:52:52 -0400

    merge

--- src/benchmark.py
+++ src/benchmark.py
@@ -27,12 +27,9 @@ def accuracyOfSuperpixels(file_num,valid_files, valid_data, clf, valid_labels,va
                 count_correct+=1
     print validationOriginalImage[file_num]
     print ('Validation Accuracy (Superpixel level): %2.2f%%')%(100.0*count_correct/total_samples)
-<<<<<<< HEAD
-    return 100.0*count_correct/total_samples
 
-=======
     return count_correct, total_samples
->>>>>>> test
+
 #@input: 2D result of the prediction
 #        2D labels
 #@output: the probility of accuracy in pixel level
@@ -44,25 +41,17 @@ def accuracyOfPixels(file_num,valid_files, superpixels, valid_data, clf, valid_p
     total_samples = indicess.shape[0]
     for i in range(0,total_samples):
         temp = np.array(superpixels[file_num][0])
-<<<<<<< HEAD
- 
+
         temp2 = np.array(valid_pixels_labels[file_num][0])
         index = np.array(np.where(temp == i)).T
 
-=======
-        temp2 = np.array(valid_pixels_labels[file_num][0])
-        index = np.array(np.where(temp == i)).T
->>>>>>> test
         total_count = total_count + index.shape[0]
         predict_result = clf.predict(valid_data[indicess[i]])
         #print predict_result == temp2[index[0:index.shape[]]]
         for j in range(0,index.shape[0]): 
             if predict_result == temp2[index[j][0]][index[j][1]]:
                count_correct+=1
-<<<<<<< HEAD
 
-    print ('Validation Accuracy (Pixel level): %2.2f%%')%(100.0*count_correct/total_count)
-=======
     print validationOriginalImage[file_num]
     print ('Validation Accuracy (Pixel level): %2.2f%%')%(100.0*count_correct/total_count)
     return count_correct, total_count
@@ -70,4 +59,4 @@ def accuracyOfPixels(file_num,valid_files, superpixels, valid_data, clf, valid_p
 def overrallAverageResult(superpixelCorrect, superpixelTotal,pixelCorrect, pixelTotal):
     print ('The Validation Set Accuracy (SuperPixel level): %2.2f%%')%(100.0*superpixelCorrect/superpixelTotal)
     print ('The Validation Set Accuracy (Pixel level): %2.2f%%')%(100.0*pixelCorrect/pixelTotal)
->>>>>>> test
+
